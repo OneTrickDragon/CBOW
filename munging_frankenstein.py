e909f0c96b02f922c9937f7152b53515e5b2940d
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2025-12-26 01:06:21 -0500

    munging

--- munging_frankenstein.py
+++ munging_frankenstein.py
@@ -9,3 +9,12 @@ import re
 import string
 from tqdm.notebook import tqdm
 
+args = Namespace(
+    raw_dataset_txt="data/books/frankenstein.txt",
+    window_size=5,
+    train_proportion=0.7,
+    val_proportion=0.15,
+    test_proportion=0.15,
+    output_munged_csv="data/books/frankenstein_with_splits.csv",
+    seed=9248
+)
